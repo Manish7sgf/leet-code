803c180b78cee8047cf4a692ac431c0ac89dc25e
Manish Varman <manishvarman5@gmail.com>
2026-07-14 14:18:13 +0530

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -0,0 +1,22 @@
+// Last updated: 7/14/2026, 2:18:14 PM
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans=new ArrayList<>();
+        Map<String,List<String>> hm=new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            String s=strs[i];
+            char[]arr=s.toCharArray();
+            Arrays.sort(arr);
+            String keyStr=new String(arr);
+            if(!hm.containsKey(keyStr)){
+                List<String> tt=new ArrayList<>();
+                hm.put(keyStr,tt);
+            }
+            hm.get(keyStr).add(s);
+        }
+        for(String key:hm.keySet()){
+            ans.add(hm.get(key));
+        }
+        return ans;
+    }
+}
