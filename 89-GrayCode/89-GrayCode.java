bc09cd9b0a77b79956877359195642b40b792942
Manish Varman <manishvarman5@gmail.com>
2026-07-14 14:17:56 +0530

    Create 89-GrayCode

---
+++ 89-GrayCode/89-GrayCode.java
@@ -0,0 +1,8 @@
+// Last updated: 7/14/2026, 2:17:56 PM
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new LinkedList<>();
+        for (int i = 0; i < 1<<n; i++) result.add(i ^ i>>1);
+        return result;
+    }
+}
