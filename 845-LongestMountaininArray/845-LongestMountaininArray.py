77e045863216e242c86bceb4300a52eec07e9474
Manish Varman <manishvarman5@gmail.com>
2026-07-27 12:29:40 +0530

    File updated at 7/27/2026, 12:29:39 PM

--- 845-LongestMountaininArray/845-LongestMountaininArray.py
+++ 845-LongestMountaininArray/845-LongestMountaininArray.py
@@ -1,6 +1,17 @@
-# Last updated: 7/25/2026, 11:57:28 AM
-1class Solution:
-2    def longestMountain(self, a: List[int]) -> int:
-3        return max((k+l+1 for (q,k),(p,l) in pairwise(
-4            (q,abs(sum(g))) for q,g in groupby((v<u)-(v>u) for v,u in pairwise(a))) 
-5                if q==1==-p),default=0)
+# Last updated: 7/27/2026, 12:29:39 PM
+1from typing import List
+2class Solution:
+3    def checkPossibility(self, nums:List[int]) -> bool:
+4        n=len(nums)
+5        chan=0
+6        for i in range(n-1):
+7            if nums[i]>nums[i+1]:
+8                chan+=1
+9                if chan>1:
+10                    return False
+11                
+12                if i==0 or nums[i-1]<=nums[i+1]:
+13                    nums[i]=nums[i+1]
+14                else:
+15                    nums[i+1]=nums[i]
+16        return True
