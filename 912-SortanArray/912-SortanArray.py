59998e57ecaf1abccab097f5b6e425d90cff740d
Manish Varman <manishvarman5@gmail.com>
2026-07-28 13:48:30 +0530

    File updated at 7/28/2026, 1:48:29 PM

--- 912-SortanArray/912-SortanArray.py
+++ 912-SortanArray/912-SortanArray.py
@@ -1,5 +1,14 @@
-# Last updated: 7/27/2026, 11:41:41 AM
+# Last updated: 7/28/2026, 1:48:29 PM
 1class Solution:
-2    def sortArray(self, nums: List[int]) -> List[int]:
-3        nums.sort()
-4        return nums
+2    def productExceptSelf(self, nums: List[int]) -> List[int]:
+3        n=len(nums)
+4        res=[0]*n
+5        pre=1
+6        for i in range(n):
+7            res[i]=pre
+8            pre*=nums[i]
+9        suf=1
+10        for i in range(n-1,-1,-1):
+11            res[i]*=suf
+12            suf*=nums[i]
+13        return res
