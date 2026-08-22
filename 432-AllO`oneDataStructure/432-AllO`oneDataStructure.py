7e21c8030c9532949a313b64568a128779354a24
Manish Varman <manishvarman5@gmail.com>
2026-08-22 12:08:08 +0530

    File updated at 8/22/2026, 12:08:05 PM

--- 432-AllO`oneDataStructure/432-AllO`oneDataStructure.py
+++ 432-AllO`oneDataStructure/432-AllO`oneDataStructure.py
@@ -1,25 +1,13 @@
-# Last updated: 8/22/2026, 10:16:50 AM
-1class Solution:
-2    def threeSum(self, nums: List[int]) -> List[List[int]]:
-3        nums.sort()
-4        result = []
-5        for i in range(len(nums) - 2):
-6            if i > 0 and nums[i] == nums[i - 1]:
-7                continue
-8            left = i + 1
-9            right = len(nums) - 1
-10            while left < right:
-11                total = nums[i] + nums[left] + nums[right]
-12                if total == 0:
-13                    result.append([nums[i], nums[left], nums[right]])
-14                    while left < right and nums[left] == nums[left + 1]:
-15                        left += 1
-16                    while left < right and nums[right] == nums[right - 1]:
-17                        right -= 1
-18                    left += 1
-19                    right -= 1
-20                elif total < 0:
-21                    left += 1
-22                else:
-23                    right -= 1
-24        return result
+# Last updated: 8/22/2026, 12:08:05 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+9        queue=deque([root])
+10        while node:=queue.popleft():
+11            queue.extend([node.left,node.right])
+12        return not any(queue)
