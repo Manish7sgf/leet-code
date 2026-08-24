a1deccefdefdc06956e19aa20c5c50dc9d9eb995
Manish Varman <manishvarman5@gmail.com>
2026-08-24 12:17:59 +0530

    File updated at 8/24/2026, 12:17:56 PM

--- 450-DeleteNodeinaBST/450-DeleteNodeinaBST.py
+++ 450-DeleteNodeinaBST/450-DeleteNodeinaBST.py
@@ -1,20 +1,25 @@
-# Last updated: 8/22/2026, 12:10:23 PM
-1class Solution:
-2    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-3        if root is None:
-4            return None
-5        if key < root.val:
-6            root.left = self.deleteNode(root.left, key)
-7        elif key > root.val:
-8            root.right = self.deleteNode(root.right, key)
-9        else:
-10            if root.left is None:
-11                return root.right
-12            if root.right is None:
-13                return root.left
-14            temp = root.right
-15            while temp.left:
-16                temp = temp.left
-17            root.val = temp.val
-18            root.right = self.deleteNode(root.right, temp.val)
-19        return root
+# Last updated: 8/24/2026, 12:17:56 PM
+1"""
+2# Definition for a Node.
+3class Node:
+4    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+5        self.val = int(x)
+6        self.next = next
+7        self.random = random
+8"""
+9
+10class Solution:
+11    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+12        if head is None:
+13            return None
+14        mp={}
+15        curr=head
+16        while curr:
+17            mp[curr]=Node(curr.val)
+18            curr=curr.next
+19        curr=head
+20        while curr:
+21            mp[curr].next= mp.get(curr.next)
+22            mp[curr].random=mp.get(curr.random)
+23            curr=curr.next
+24        return mp[head]
