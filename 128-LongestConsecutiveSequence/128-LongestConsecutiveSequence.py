6765f13be23cae7cceea6e6f40c10b1e030d6b37
Manish Varman <manishvarman5@gmail.com>
2026-08-28 10:59:44 +0530

    File updated at 8/28/2026, 10:59:43 AM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
@@ -1,14 +1,14 @@
-# Last updated: 8/24/2026, 11:48:14 AM
+# Last updated: 8/28/2026, 10:59:43 AM
 1class Solution:
-2    def longestConsecutive(self, nums: List[int]) -> int:
-3        s=set(nums)
-4        longest=0
-5        for i in s:
-6            if i-1 not in s:
-7                curr=i
-8                length=1
-9                while curr+1 in s:
-10                    curr+=1
-11                    length+=1
-12                longest=max(longest,length)
-13        return longest
+2    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+3        r={0:-1}
+4        t=0
+5        for i in range(len(nums)):
+6            t+=nums[i]
+7            rem=t%k
+8            if rem in r:
+9                if i-r[rem]>=2:
+10                    return True
+11            else:
+12                r[rem]=i
+13        return False
