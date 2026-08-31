236b75febd02d836b69cfea8a26e0be9e69d4404
Manish Varman <manishvarman5@gmail.com>
2026-08-31 13:57:59 +0530

    Create 875-LongestMountainInArray

---
+++ 875-LongestMountainInArray/875-LongestMountainInArray.py
@@ -0,0 +1,6 @@
+# Last updated: 8/31/2026, 1:57:59 PM
+class Solution:
+    def longestMountain(self, a: List[int]) -> int:
+        return max((k+l+1 for (q,k),(p,l) in pairwise(
+            (q,abs(sum(g))) for q,g in groupby((v<u)-(v>u) for v,u in pairwise(a))) 
+                if q==1==-p),default=0)
