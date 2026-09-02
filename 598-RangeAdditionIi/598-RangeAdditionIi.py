b9c13d003c96c19bf9900d1a3b9b06ad64e9f322
Manish Varman <manishvarman5@gmail.com>
2026-09-02 17:48:06 +0530

    Create 598-RangeAdditionIi

---
+++ 598-RangeAdditionIi/598-RangeAdditionIi.py
@@ -0,0 +1,8 @@
+# Last updated: 9/2/2026, 5:48:06 PM
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        if not ops:
+            return m*n
+        minX=min(op[0] for op in ops)
+        minY=min(op[1] for op in ops)
+        return minX*minY
