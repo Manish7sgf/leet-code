9d8494dab98880a5cac71fa64eb870b05f0a2fc4
Manish Varman <manishvarman5@gmail.com>
2026-09-18 10:14:01 +0530

    Create 32-LongestValidParentheses

---
+++ 32-LongestValidParentheses/32-LongestValidParentheses.py
@@ -0,0 +1,15 @@
+# Last updated: 9/18/2026, 10:13:58 AM
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack=[-1]
+        ans=0
+        for i in range(len(s)):
+            if s[i]=='(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    ans=max(ans,i-stack[-1])
+        return ans
